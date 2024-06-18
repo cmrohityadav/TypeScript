a8a7ee74fccb199a567e472154acc8a3ccbaacd0
cmrohityadav <cmrohityadav23@gmail.com>
2024-06-18 13:25:07 +0530

    readonly

---
+++ 01_Basic/08_readonlyAndOptional.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var User1 = {
+    _id: "123",
+    name: "rohit",
+    email: "r@rohit.com",
+    mobile: 123456,
+};
+User1.email = "cm@r";
---
+++ 01_Basic/08_readonlyAndOptional.ts
@@ -0,0 +1,20 @@
+type User={
+    readonly _id:string
+    name:string;
+    email:string;
+    mobile:number;
+    addhar?:number
+}
+
+const User1:User={
+    _id: "123",
+    name:"rohit",
+    email:"r@rohit.com",
+    mobile:123456,
+
+}
+
+User1.email="cm@r"
+
+
+export {}
