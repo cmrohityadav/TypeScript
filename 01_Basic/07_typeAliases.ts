0d8c7ec7f54569e165d3cb99bb0f7bf7810a0cd5
cmrohityadav <cmrohityadav23@gmail.com>
2024-06-18 13:14:11 +0530

    type aliases

---
+++ 01_Basic/07_typeAliases.ts
@@ -0,0 +1,15 @@
+type User={
+    name:string;
+    email:string;
+    isActive:boolean;
+}
+
+function createUser(user:User){
+
+}
+const rahul={
+    name:"Rohit",
+    email:"cm@rohit",
+    isActive:true
+}
+createUser(rahul)
