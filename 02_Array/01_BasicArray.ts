f82ed9ef7f6f8e7221f8f528a205fcc29f26bb04
cmrohityadav <cmrohityadav23@gmail.com>
2024-06-19 13:08:38 +0530

    basic array

---
+++ 02_Array/01_BasicArray.ts
@@ -0,0 +1,15 @@
+const superHeroes:string[]=[]
+
+superHeroes.push("skatiman")
+
+const river:Array<string>=[]
+river.push("ganga")
+
+type User={
+    name:string
+    age:number
+}
+
+const alluser:User[]=[]
+// alluser.push("") erro
+alluser.push({name:"rohit",age:23})
