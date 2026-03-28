0c18690b753250e77bb25610ca24db4d364d8120
cmrohityadav <cmrohityadav23@gmail.com>
2026-03-28 13:19:35 +0530

    added setup and working of ts

---
+++ 00_setup/src/index.ts
@@ -0,0 +1,8 @@
+function welcomeExchange(userId:string,exchange:string): string{
+    return `Hey ${userId}, Welocome to ${exchange}`;
+}
+
+const userid:string ="ROHIT";
+const exchangeName:string="MSEI";
+
+console.log(welcomeExchange(userid,exchangeName));
